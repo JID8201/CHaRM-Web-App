0efbc98b7f59e21eb5db98ba6d5fd91057e03162
jhumphreis30 <jhumphries30@gatech.edu>
2018-09-28 15:19:46 -0400

    Firebase integration with the backend set up. I suck at Javascript though so it needs some work

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,22 @@
 "use strict";
+var admin = require("firebase-admin");
+var serviceAccount = require("./charm-f4f06-firebase-adminsdk-a15sk-443f9222bb.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://charm-f4f06.firebaseio.com"
+});
+
+var db = admin.database();
+var ref = db.ref("orders/")
+
+// example of how to get the data a process it
+ref.on('value', function(snapshot) {
+  var data = snapshot.val();
+  var keys = Object.keys(data);
+  console.log(data);
+})
+
 
 const express = require('express');
 const app = express();
