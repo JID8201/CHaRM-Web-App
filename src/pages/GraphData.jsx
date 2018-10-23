8635d4e986be45310cfb94541ff8af9492db6fc0
Olivia Powell <olive.p@outlook.com>
2018-10-22 20:46:25 -0400

    Graphs are now beautiful AF

--- src/pages/GraphData.jsx
+++ src/pages/GraphData.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import Graph from '../components/Graph';
+import {Button} from '@material-ui/core';
 
 class GraphData extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            graph: 'donut'
+        }
+    }
+
+    handleGraphChange = (graph) => {
+        this.setState({ graph: graph })
+    }
+
     render() {
         return (
-            <Graph />
+            <div>
+                <Button onClick={() => this.handleGraphChange('donut')}>Donut</Button>
+                <Button onClick={() => this.handleGraphChange('bar')}>Bar</Button>
+                <Graph graph={this.state.graph}/>
+            </div>
         );
     }
 }
