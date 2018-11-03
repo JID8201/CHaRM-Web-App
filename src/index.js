1ff742cc862a1d0856bc5393ad454a7c1cf6f60c
Olivia Powell <olive.p@outlook.com>
2018-11-03 14:28:32 -0400

    casing

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import './assets/styles/index.css'
 import App from './pages/App'
 import registerServiceWorker from './registerServiceWorker'
-import appState from './stores/appState'
+import AppState from './stores/appState'
 import recyclingStore from './stores/recyclingStore'
 import { Provider } from 'mobx-react'
 import { BrowserRouter } from 'react-router-dom'
@@ -11,7 +11,7 @@ import { MuiThemeProvider } from '@material-ui/core/styles'
 import theme from './assets/styles/theme'
 
 const stores = {
-  appState,
+  AppState,
   recyclingStore
 }
 
