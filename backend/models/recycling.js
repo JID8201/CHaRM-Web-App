8cdf06e1b494c0be6491a7224dcde59e598638ee
Olivia Powell <olive.p@outlook.com>
2018-11-03 15:46:03 -0400

    updated notes in model to string not string array

--- backend/models/recycling.js
+++ backend/models/recycling.js
@@ -1,28 +1,28 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 var RecyclingSchema = new mongoose.Schema({
-    type: {
-        type: String,
-        required: true
-    },
-    zip: {
-        type: Number,
-        min: 0o0000,
-        max: 99999,
-        required: true
-    },
-    amount: {
-        type: Number,
-        required: true
-    },
-    notes: {
-        type: [String],
-        required: false
-    }
+  type: {
+    type: String,
+    required: true
+  },
+  zip: {
+    type: Number,
+    min: 0o0000,
+    max: 99999,
+    required: true
+  },
+  amount: {
+    type: Number,
+    required: true
+  },
+  notes: {
+    type: String,
+    required: false
+  }
 }, {
-    timestamps: {
-        createdAt: 'created_at'
-    }
+  timestamps: {
+    createdAt: 'created_at'
+  }
 })
 
 module.exports = mongoose.model('Recycling', RecyclingSchema)
