db78b9df11b8bbf67c6b4d578097741009fd6ea8
Olivia Powell <olive.p@outlook.com>
2018-11-08 22:36:00 -0500

    Fixed styling

--- backend/controllers/recycling.js
+++ backend/controllers/recycling.js
@@ -1,79 +1,79 @@
-const Recycling = require('mongoose').model('Recycling');
+const Recycling = require('mongoose').model('Recycling')
 
 module.exports.create = (req, res, next) => {
-    if (!req.body.type) {
-        return next(new Error('type of recycled item is required'))
+  if (!req.body.type) {
+    return next(new Error('type of recycled item is required'))
+  }
+  if (!req.body.zip) {
+    return next(new Error('zip code is required'))
+  }
+  if (!req.body.amount) {
+    return next(new Error('amount of recycled item is required'))
+  }
+
+  Recycling.create({
+    type: req.body.type,
+    zip: req.body.zip,
+    amount: req.body.amount,
+    notes: req.body.notes ? req.body.notes : []
+  }, (err, result) => {
+    if (err) {
+      res.locals.error = {
+        status: 500,
+        msg: 'error: ' + err
+      }
+      return next()
+    } else {
+      res.locals.data = {
+        recycling: result,
+        msg: 'Recycling data successfully added'
+      }
+      return next()
     }
-    if (!req.body.zip) {
-        return next(new Error('zip code is required'))
-    }
-    if (!req.body.amount) {
-        return next(new Error('amount of recycled item is required'))
-    }
-    
-    Recycling.create({
-        type: req.body.type,
-        zip: req.body.zip,
-        amount: req.body.amount,
-        notes: req.body.notes ? req.body.notes : []
-    }, (err, result) => {
-        if (err) {
-            res.locals.error = {
-                status: 500, 
-                msg: 'error: ' + err
-            }
-            return next()
-        } else {
-            res.locals.data = {
-                recycling: result,
-                msg: 'Recycling data successfully added'
-            }
-            return next()
-        }
-    })
+  })
 }
 
 module.exports.getDateRange = (req, res, next) => {
-    // this is bad, shouldn't throw error should return dates in the given range or all
-    if (!req.query.startDate) {
-        return next(new Error('Starting date range required'))
-    }
-    if (!req.query.endDate) {
-        return next(new Error('Ending date range required'))
-    }
-    const start = new Date(req.query.startDate)
-    const end = new Date(req.query.endDate)
-    Recycling.find({created_at: {"$gte": start, "$lte": end}},
-        (err, result) => {
-            if (err) {
-                // we done fucked up bad
-                res.locals.error = {
-                    status: 500,
-                    msg: 'error: ' + err
-                }
-                return next()
-            } else {
-                res.locals.data = {
-                    recycling: result
-                }
-                return next()
-            }
+  // this is bad, shouldn't throw error should return dates in the given range or all
+  if (!req.query.startDate) {
+    return next(new Error('Starting date range required'))
+  }
+  if (!req.query.endDate) {
+    return next(new Error('Ending date range required'))
+  }
+  const start = new Date(req.query.startDate)
+  const end = new Date(req.query.endDate)
+  Recycling.find({created_at: {'$gte': start, '$lte': end}},
+    (err, result) => {
+      if (err) {
+        // we done fucked up bad
+        res.locals.error = {
+          status: 500,
+          msg: 'error: ' + err
         }
-    )
+        return next()
+      } else {
+        res.locals.data = {
+          recycling: result
+        }
+        return next()
+      }
+    }
+  )
 }
 
 module.exports.get = (req, res, next) => {
-    Recycling.find({}, (err, result) => {
-        if (err) {
-            res.locals.error = {
-                status: 500,
-                msg: 'error: ' + err
-            }
-            return next()
-        } else {
-            res.locals.data = {
-                recycling: result
-            }
-        }
-    })
+  Recycling.find({}, (err, result) => {
+    if (err) {
+      res.locals.error = {
+        status: 500,
+        msg: 'error: ' + err
+      }
+      return next()
+    } else {
+      res.locals.data = {
+        recycling: result
+      }
+    }
+  })
 }
